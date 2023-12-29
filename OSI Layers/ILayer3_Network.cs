297b6c80f415270df284bbe71621b5d249b6857e
SMG3zx <nerfsteven@gmail.com>
2023-12-29 15:46:39 +0300

    Added Layer 3 Network Note

--- OSI Layers/ILayer3_Network.cs
+++ OSI Layers/ILayer3_Network.cs
@@ -1,6 +1,22 @@
-namespace NetworkSimulator.OSI_Layers;
+namespace CyberSimulator.OSI_Layers;
 
-public interface ILayer3_Network
+/*
+ * The network layer is responsible for managing the routing of data across multiple networks.
+ * Its main function is providing logical addressing and routing services.
+ * When it receives data from the data link layer it encapsulates it into packets which are then routed.
+ * The network layer also provides services such as fragmentation and reassembly with MTU (Maximum Transmission Unit) sizes.
+ * It also provides QOS and congestion control
+ */
+
+// The network layer uses the following technologies:
+// IP (Internet Protocol)
+// ICMP (Internet Control Message Protocol)
+// ARP (Address Resolution Protocol)\
+// OSPF (Open Shortest Path First)
+// BGP (Border Gateway Protocol)
+// IPSEC (Internet Protocol Security)
+
+public interface ILayer3Network
 {
     
 }
