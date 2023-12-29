32ba6e69bbcb2cab261af0d704dd1417edc1998b
SMG3zx <nerfsteven@gmail.com>
2023-12-29 16:16:55 +0300

    Added Layer7_Application Note

--- OSI Layers/ILayer7_Application.cs
+++ OSI Layers/ILayer7_Application.cs
@@ -1,6 +1,17 @@
-namespace NetworkSimulator.OSI_Layers;
+namespace CyberSimulator.OSI_Layers;
 
-public interface ILayer7_Application
+/*
+ * The application layer is the only layer that directly interacts with data from the user.
+ * Software applications like web browsers and email clients rely on the application layer to initiate communications.
+ * It should be noted that the application layer is not the same as an application, but rather that the application layer is responsible for the protocols and data manipulation that applications rely on.
+ * In so doing presenting meaningful data to the user.
+ */
+
+// Some examples of application layer protocols include:
+// HTTP (HyperText Transfer Protocol) 
+// SMTP (Simple Mail Transfer Protocol)
+
+public interface ILayer7Application
 {
     
 }
