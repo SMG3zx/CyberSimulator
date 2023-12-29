f5374f74c04f83557479fe6f81ce54ffccd0a354
SMG3zx <nerfsteven@gmail.com>
2023-12-29 16:13:33 +0300

    Added Layer6_Presentation Note

--- OSI Layers/ILayer6_Presentation.cs
+++ OSI Layers/ILayer6_Presentation.cs
@@ -1,6 +1,19 @@
-namespace NetworkSimulator.OSI_Layers;
+namespace CyberSimulator.OSI_Layers;
 
-public interface ILayer6_Presentation
+/*
+ * The presentation layer is responsible for ensuring that data is presented in a format that can be understood but the receiving application.
+ * The layer provides services such as data conversion, data compression, encryption, and decryption.
+ * It translates from an internal view to a transmission view.
+ */
+
+// Protocols on the layer include
+// ASCII
+// Unicode
+// JPEG
+// MPEG
+// SSL/TLS
+
+public interface ILayer6Presentation
 {
     
 }
