d441d34c780a0ba36d9452c96dcb84f4b84f6cab
SMG3zx <nerfsteven@gmail.com>
2023-12-29 15:55:58 +0300

    Added Layer5_Session note

--- OSI Layers/ILayer5_Session.cs
+++ OSI Layers/ILayer5_Session.cs
@@ -1,6 +1,19 @@
-namespace NetworkSimulator.OSI_Layers;
+namespace CyberSimulator.OSI_Layers;
 
-public interface ILayer5_Session
+/*
+ * The session layer is responsible for establishing, managing, and terminating sessions between applications on different hosts on a network.
+ * In addition it also handles synchronization and recovery.
+ * This layer uses protocols and mechanisms such as session checkpoints and recovery, encryption and decryption.
+ */
+
+// This layers protocols include: 
+// NetBIOS (Network Basic Input/Output System)
+// RPC (Remote Procedure Call)
+// ATP (AppleTalk Transaction Protocol)
+// SOCKS (Socket Secure)
+
+
+public interface ILayer5Session
 {
     
 }
