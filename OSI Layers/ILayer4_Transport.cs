06593a646eb7bd661decd83ffdc162d7bfac549a
SMG3zx <nerfsteven@gmail.com>
2023-12-29 15:50:40 +0300

    Added Layer4_Transport Note

--- OSI Layers/ILayer4_Transport.cs
+++ OSI Layers/ILayer4_Transport.cs
@@ -1,6 +1,14 @@
-namespace NetworkSimulator.OSI_Layers;
+namespace CyberSimulator.OSI_Layers;
 
-public interface ILayer4_Transport
+/*
+ * The transport layer provides end to end communication between applications of different endpoints.
+ * Its main function is to ensure that data is transferred reliably and efficiently between endpoints.
+ * This function is achieved by using 2 main protocols: TCP and UDP.
+ * TCP being the connection oriented side using a 3 way handshake.
+ * UDP being the connectionless protocol with less overhead, aimed for fast paced comms where packet loss is not detrimental.
+ */
+
+public interface ILayer4Transport
 {
     
 }
