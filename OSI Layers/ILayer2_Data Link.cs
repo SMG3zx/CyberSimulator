cf8746e4c9424b9e9b3f7c50a8cb7a55d26c2f25
SMG3zx <nerfsteven@gmail.com>
2023-12-29 15:36:50 +0300

    Added Layer2_DataLink Note

--- OSI Layers/ILayer2_Data Link.cs
+++ OSI Layers/ILayer2_Data Link.cs
@@ -1,6 +1,12 @@
 namespace NetworkSimulator.OSI_Layers;
 
-public interface ILayer2_Data_Link
+/*
+ * This layer provides error-free transmission of data across a physical link, using technologies such as error detection and correction.
+ * The layer focuses on local delivery of frames between nodes on the same network segment and uses protocols such as CSMA/CD (Carrier Sense Multiple Access with Collision Detection) in ethernet networks
+ * along with the Binary Exponential Backoff Algorithm to prevent further collisions alongside preventing the problem from happening in the first place.
+ * Popular data link protocols include ethernet, IEEE 802.11 (Wi-Fi), ATM (Asynchronous Transfer Mode), and frame relay.
+ */
+public interface ILayer2DataLink
 {
     
 }
